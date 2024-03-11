a9dccad4afa06bdddabc134ee2fe3bde672b8547
Amora14 <amira.hsleiman@gmail.com>
2024-03-11 04:53:52 -0300

    repositorio do produto arrumado

--- src/main/java/app/repository/ProdutoRepository.java
+++ src/main/java/app/repository/ProdutoRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import app.entity.Funcionario;
 import app.entity.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
